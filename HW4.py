78809f828331c010c309ae153e1cb40de266844a
Irina <andrey_k.90@mail.ru>
2022-09-18 00:27:23 +0300

    Решение 4 задачи

--- HW4.py
+++ HW4.py
@@ -36,16 +36,44 @@
 # 3. Задайте последовательность чисел. Напишите программу, которая выведет список
 # неповторяющихся элементов исходной последовательности.
 
-ex_dann = input('Введите числа через пробел:')
-list = []
-list = ex_dann.split(' ')
-list = [int(ex_dann) for ex_dann in list]
-print('исходный список', list)
-dubb = [x for i, x in enumerate(list) if x in list[:i]]
-print('повтор:', dubb)
-list_res = []
-for numb in list:
-    if numb not in dubb:
-        list_res.append(numb)
-print(list_res)
+# ex_dann = input('Введите числа через пробел:')
+# list = []
+# list = ex_dann.split(' ')
+# list = [int(ex_dann) for ex_dann in list]
+# print('исходный список', list)
+# dubb = [x for i, x in enumerate(list) if x in list[:i]]
+# print('повтор:', dubb)
+# list_res = []
+# for numb in list:
+#     if numb not in dubb:
+#         list_res.append(numb)
+# print(list_res)
 
+# 4. Задана натуральная степень k. Сформировать случайным образом список коэффициентов
+# (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+
+# Пример:
+
+# - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+
+import random
+k= int(input('введите число: '))
+s=str()
+
+for poww in range(0,k+1):
+  coeff=random.randint(0, 100)
+  
+  coeff=str(coeff)
+  
+  if poww==0:
+    s=s+coeff+'+'
+  else:
+   
+    if poww==k:
+     s=s+coeff+'*x**'+str(poww)+'=0'
+    else:
+      s=s+coeff+'*x**'+str(poww)+'+'
+print(s)
+  
+    
